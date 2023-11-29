caa33b4a7debff622b63c6072bcde1d46d52bc27
Clemana <ananaclem@gmail.com>
2023-11-29 14:14:08 +0100

    ajout filtre des travaux

---
+++ FrontEnd/assets/Javascript/index.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.querySelector('.gallery');
+  let allWorks = []; // 
+
+  async function fetchData() {
+    try {
+      const [worksResponse, categoriesResponse] = await Promise.all([
+        fetch('http://localhost:5678/api/works'),
+        fetch('http://localhost:5678/api/categories')
+      ]);
+
+      if (!worksResponse.ok || !categoriesResponse.ok) {
+        throw new Error('Erreur HTTP dans au moins l\'une des requêtes');
+      }
+
+      const [worksData, categoriesData] = await Promise.all([
+        worksResponse.json(),
+        categoriesResponse.json()
+      ]);
+
+      allWorks = worksData; 
+      integrerProjets(allWorks, gallery);
+      displayCategories(categoriesData);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données:', error);
+    }
+  }
+
+  function integrerProjets(projets, gallery) {
+    gallery.innerHTML = '';
+
+    projets.forEach(projet => {
+      const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      const figcaption = document.createElement('figcaption');
+
+      img.src = projet.imageUrl;
+      img.alt = projet.title;
+
+      figcaption.textContent = projet.title;
+
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      gallery.appendChild(figure);
+    });
+  }
+
+  function displayCategories(categories) {
+    const filtersContainer = document.createElement('div');
+    filtersContainer.id = 'filters-container';
+
+    categories.forEach(category => {
+      const filterButton = document.createElement('button');
+      filterButton.textContent = category.name;
+      filterButton.addEventListener('click', () => {
+        filterWorksByCategory(category.id);
+      });
+
+      filtersContainer.appendChild(filterButton);
+    });
+
+    
+    const portfolioSection = document.getElementById('portfolio');
+    portfolioSection.insertBefore(filtersContainer, portfolioSection.firstChild);
+  }
+
+  function filterWorksByCategory(categoryId) {
+    const filteredWorks = allWorks.filter(work => work.categoryId === categoryId);
+    integrerProjets(filteredWorks, gallery);
+  }
+
+  
+  fetchData();
+});
+
